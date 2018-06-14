93b3419e63917e69b59ff93ea1fa81596b74b853
MoonHyoJong <gywhd408@naver.com>
2018-06-14 17:17:21 +0900

    카메라 기능 추가

--- app/src/main/java/com/example/hyojong/dkeis/ChangeActivity.java
+++ app/src/main/java/com/example/hyojong/dkeis/ChangeActivity.java
@@ -1,17 +1,45 @@
 package com.example.hyojong.dkeis;
 
 import android.app.ProgressDialog;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+/*
+import com.kakao.AppActionBuilder;
+import com.kakao.KakaoLink;
+import com.kakao.KakaoParameterException;
+import com.kakao.KakaoTalkLinkMessageBuilder;
+import com.kakao.exception.KakaoException;
+*/
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+
 
 public class ChangeActivity extends AppCompatActivity {
 
@@ -19,23 +47,163 @@ public class ChangeActivity extends AppCompatActivity {
     private ImageView changeImage;
     private String msg;
     private String userURL;
+    private Button downloadBtn;
+    private ImageButton kakaoBtn;
+    Bitmap mSaveBm;
+
+    /*
+    private KakaoLink kakaoLink;
+    private KakaoTalkLinkMessageBuilder kakaoTalkLinkMessageBuilder;
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change);
+        final String kakaoURL = "http://114.70.235.44:18081/capstone/style/1.jpg";
+        final String kakaoText = "테스트";
+        final String kakaoURLtest = "http://114.70.235.44:18081/capstone/style/1.jpg";
 
+        /*
+        try {
+            kakaoLink = KakaoLink.getKakaoLink(ChangeActivity.this);
+            kakaoTalkLinkMessageBuilder = kakaoLink.createKakaoTalkLinkMessageBuilder();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* 카카오 링크 API *
+        kakaoBtn = (ImageButton)findViewById(R.id.kakaoBtn);
+        kakaoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    kakaoTalkLinkMessageBuilder.addText(kakaoText);
+                    kakaoTalkLinkMessageBuilder.addImage(kakaoURL, 320, 320);
+                    kakaoTalkLinkMessageBuilder.addWebLink(kakaoURLtest);
+                    kakaoTalkLinkMessageBuilder.addAppButton("앱열기",
+                            new AppActionBuilder().setAndroidExecuteURLParam("target=main").
+                                    setIOSExecuteURLParam("target=main",AppActionBuilder.DEVICE_TYPE.PHONE).build());
+                } catch (KakaoParameterException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+
+        /*
         msg = getIntent().getStringExtra("msg");
+        changeText = (TextView)findViewById(R.id.changeText);
+        changeText.setText(msg);*/
+
+        //  msg = getIntent().getStringExtra("msg");
         userURL = getIntent().getStringExtra("userURL");
         changeText = (TextView)findViewById(R.id.changeText);
-        changeText.setText(msg);
+        // changeText.setText(msg);
+
+        downloadBtn = (Button)findViewById(R.id.downloadBtn);
 
         LoadImageTask loadImageTask = new LoadImageTask();
         loadImageTask.execute();
 
         changeImage = (ImageView)findViewById(R.id.changeImage);
 
+        BitmapFactory.Options bmOptions;
+        bmOptions = new BitmapFactory.Options();
+        bmOptions.inSampleSize = 1;
 
+        OpenHttpConnection opHttpCon = new OpenHttpConnection();
+        opHttpCon.execute(changeImage, userURL);
+
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String storeName;
+                OutputStream outStream = null;
+
+                storeName = userURL.split("/")[userURL.split("/").length-1];
+
+                String extStorageDirectory =  Environment.getExternalStorageDirectory().getAbsolutePath() + "/DKEIS/";
+
+                //System.out.println("**********DIR: " + extStorageDirectory);
+                File file = new File(extStorageDirectory, storeName);
+                //storeCropImage(GetImageFromURL(userURL),extStorageDirectory);
+
+                try {
+                    outStream = new FileOutputStream(file);
+                    mSaveBm.compress(
+                            Bitmap.CompressFormat.PNG, 100, outStream);
+                    outStream.flush();
+                    outStream.close();
+
+                    Toast.makeText(ChangeActivity.this,
+                            "Saved", Toast.LENGTH_LONG).show();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                    Toast.makeText(ChangeActivity.this,
+                            e.toString(), Toast.LENGTH_LONG).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(ChangeActivity.this,
+                            e.toString(), Toast.LENGTH_LONG).show();
+                }
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA,
+                        extStorageDirectory + storeName);
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+                values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+                getContentResolver().insert(
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+
+                /*Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(userURL));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);*/
+                Toast.makeText(ChangeActivity.this, "저장", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+    private Bitmap GetImageFromURL(String imgaeURL) {
+        Bitmap imgBitmap = null;
+
+        try {
+            URL url = new URL(imgaeURL);
+            URLConnection conn = url.openConnection();
+            conn.connect();
+
+            int nSize = conn.getContentLength();
+            BufferedInputStream bis = new BufferedInputStream(conn.getInputStream(), nSize);
+            imgBitmap = BitmapFactory.decodeStream(bis);
+
+            bis.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  imgBitmap;
+    }
+    private void storeCropImage(Bitmap bitmap, String filePath){
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DKEIS";
+        File directory_DKEIS = new File(dirPath);
+        if(!directory_DKEIS.exists())
+            directory_DKEIS.mkdir();
+
+        File copyFile = new File(filePath);
+        BufferedOutputStream out = null;
+
+        try {
+            copyFile.createNewFile();
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+            // sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(copyFile)));
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public Boolean isExists(String URLName) {
@@ -81,6 +249,8 @@ public class ChangeActivity extends AppCompatActivity {
         protected Void doInBackground(Void... arg0) {
             while (!isExists(userURL))
                 Log.d("#######doInBackground: ", String.valueOf(isExists(userURL)));
+            // 사진 용량이 커서 사이트에서 불러올 시간이 필요..
+            try { Thread.sleep(1000); } catch (Exception e) { e.printStackTrace(); }
             return null;
         }
 
@@ -90,5 +260,35 @@ public class ChangeActivity extends AppCompatActivity {
             Glide.with(ChangeActivity.this).load(userURL).into(changeImage);
             super.onPostExecute(result);
         }
+
     }
+    private class OpenHttpConnection extends AsyncTask<Object, Void, Bitmap> {
+
+        private ImageView bmImage;
+
+        @Override
+        protected Bitmap doInBackground(Object... params) {
+            Bitmap mBitmap = null;
+            bmImage = (ImageView) params[0];
+            String url = (String) params[1];
+            InputStream in = null;
+            try {
+                in = new java.net.URL(url).openStream();
+                mBitmap = BitmapFactory.decodeStream(in);
+                in.close();
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            return mBitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bm) {
+            super.onPostExecute(bm);
+            mSaveBm = bm;
+            bmImage.setImageBitmap(bm);
+        }
+    }
+
 }
--- app/src/main/java/com/example/hyojong/dkeis/ImageTransferActivity.java
+++ app/src/main/java/com/example/hyojong/dkeis/ImageTransferActivity.java
@@ -2,9 +2,13 @@ package com.example.hyojong.dkeis;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,17 +16,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.hyojong.dkeis.Adapter.StyleActivity;
+import com.google.android.gms.common.api.Response;
 import com.koushikdutta.async.future.Future;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
-import com.koushikdutta.ion.Response;
+
 
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+
 
 public class ImageTransferActivity extends AppCompatActivity {
     private String userUid;
@@ -32,12 +40,16 @@ public class ImageTransferActivity extends AppCompatActivity {
     private ImageView selectImage;
     private ImageView styleimage;
     private ImageView changeImage;
-    private String path, userURL, styleURL;
 
+    private String path, userURL, styleURL;
+    private String filePath;
     private String absoultePath;
+
     Uri photoUri;
     private String currentPhotoPath;//실제 사진 파일 경로
     String mImageCaptureName;//이미지 이름
+
+    private final int CAMERA_REQUEST_CODE = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +73,9 @@ public class ImageTransferActivity extends AppCompatActivity {
         selectImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                takeAlbumAction();
+                //takeAlbumAction();
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
             }
         });
 
@@ -78,42 +92,43 @@ public class ImageTransferActivity extends AppCompatActivity {
         changeImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Node.js or JSP 파일 업로드[][][][][][][][][][][][][]][][][][][][][]
-                // 1) User Image 웹 서버에 업로드, 2) Style URL 전송, 3) Node js 호출?
-                //path = getPathFromURI(selectImage.set);
-                path = absoultePath;
-                //System.out.println("###############PATH: " + path);
-                File f = new File(path);
-                Future uploading = Ion.with(ImageTransferActivity.this)
-                        //.load("http://192.168.150.1:8080/upload")
-                        .load("http://114.70.234.172:3004/upload")
-                        .setMultipartFile("image", f)
-                        .setMultipartParameter("style", styleURL)
-                        .asString()
-                        .withResponse()
-                        .setCallback(new FutureCallback<Response<String>>() {
-                            @Override
-                            public void onCompleted(Exception e, Response<String> result) {
-                                try {
+                //System.out.println("****************PATH: " + absoultePath + "  styleURL : " + styleURL);
+                if (absoultePath == null || styleURL == null) {
+                    Toast.makeText(ImageTransferActivity.this, "사진 선택해 주세요", Toast.LENGTH_SHORT).show();
+                } else {
+                    path = absoultePath;
+                    File f = new File(path);
+                    com.koushikdutta.async.future.Future uploading = Ion.with(ImageTransferActivity.this)
+                            //.load("http://192.168.150.1:8080/upload")
+                            .load("http://114.70.234.172:3004/upload")
+                            .setMultipartFile("image", f)
+                            .setMultipartParameter("style", styleURL)
+                            .asString()
+                            .withResponse()
+                            .setCallback(new FutureCallback<com.koushikdutta.ion.Response<String>>() {
+                                @Override
+                                public void onCompleted(Exception e, com.koushikdutta.ion.Response<String> result) {
+                                    try {
 
-                                    userURL = "http://114.70.234.172:3004/transfer/trans-"+ (absoultePath.split("/")[absoultePath.split("/").length-1]);
-                                    //System.out.println("@@@@@@@@@@@@@@@@@@@" + userURL);
-                                    String msg = result.getResult();
+                                        userURL = "http://114.70.234.172:3004/transfer/trans-" + (absoultePath.split("/")[absoultePath.split("/").length - 1]);
+                                        //System.out.println("@@@@@@@@@@@@@@@@@@@" + userURL);
+                                        //String msg = result.getResult();
 
-                                    //Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
+                                        //Toast.makeText(getContext(), msg, Toast.LENGTH_SHORT).show();
 
-                                    Intent intent = new Intent(ImageTransferActivity.this, ChangeActivity.class);
-                                    intent.putExtra("msg", msg);
-                                    intent.putExtra("userURL", userURL);
-                                    startActivity(intent);
+                                        Intent intent = new Intent(ImageTransferActivity.this, ChangeActivity.class);
+                                        //intent.putExtra("msg", msg);
+                                        intent.putExtra("userURL", userURL);
+                                        startActivity(intent);
 
-                                } catch (Exception e1) {
-                                    e1.printStackTrace();
+                                    } catch (Exception e1) {
+                                        e1.printStackTrace();
+                                    }
+
                                 }
+                            });
 
-                            }
-                        });
-
+                }
             }
         });
     }
@@ -123,6 +138,33 @@ public class ImageTransferActivity extends AppCompatActivity {
         intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, 1);
     }
+    private void refreshMedia(File photoFile) {
+        MediaScannerConnection.scanFile(this,
+                new String[] {photoFile.getAbsolutePath()},
+                null,
+                new MediaScannerConnection.OnScanCompletedListener(){
+                    public void onScanCompleted(String path, Uri uri){
+
+                    }
+                });
+    }
+    private File createFile() throws IOException {
+        // 임시파일명 생성
+        String tempFilename = "test";
+        //""+System.currentTimeMillis();
+        // 임시파일 저장용 디렉토리 생성
+        File tempDir = new File(Environment.getExternalStorageDirectory() + "/DKEIS/");
+        if(!tempDir.exists()){
+            tempDir.mkdirs();
+        }
+        filePath = tempDir + "/" + tempFilename + ".jpg";
+        System.out.println("##### " + filePath);
+        // 실제 임시파일을 생성
+        File tempFile = new File(filePath);
+
+
+        return tempFile;
+    }
 
     @Override
     public void onActivityResult(int requestCode ,int resultCode, Intent data) {
@@ -130,18 +172,34 @@ public class ImageTransferActivity extends AppCompatActivity {
 
         switch (requestCode) {
             case 1:
-                photoUri = data.getData();
-                //styleURL = photoUri.getPath().toString();
+                try {
+                    photoUri = data.getData();
+                } catch (Exception e) {
+                    e.printStackTrace(); break;
+                }
+
                 Log.d("Get Photo", photoUri.getPath().toString());
+
+                String filepaths = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DKEIS/" + System.currentTimeMillis() +".jpg";
+                System.out.println("CASE 2 ");
+
+                try {
+                    Bitmap photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(data.getData().toString()));
+                    photo = Bitmap.createScaledBitmap(photo, photo.getWidth()/2, photo.getHeight()/2, true);
+                    selectImage.setImageBitmap(photo);
+                    storeCropImage(photo, filepaths);
+
+                    absoultePath = filepaths;
+                    System.out.println("#########FILEPATH:" + absoultePath);
+                } catch (IOException e) { e.printStackTrace(); }
+
+                break;
+            // CROP -> 이미지 깨짐 현상 -> 사용 X
             case 0:
 
                 Intent intent = new Intent("com.android.camera.action.CROP");
                 intent.setDataAndType(photoUri, "image/*");
 
-                intent.putExtra("outputX", 200);
-                intent.putExtra("outputY",200);
-                intent.putExtra("aspectX", 1);
-                intent.putExtra("aspectY", 1);
                 intent.putExtra("scale", true);
                 intent.putExtra("return-data",true);
                 startActivityForResult(intent, 2);
@@ -149,25 +207,42 @@ public class ImageTransferActivity extends AppCompatActivity {
                 break;
 
             case 2:
-                final Bundle extras = data.getExtras();
                 String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/DKEIS/" + System.currentTimeMillis() +".jpg";
 
-                if(extras != null) {
-                    Bitmap photo = extras.getParcelable("data");
+                try {
+                    Bitmap photo = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(data.getData().toString()));
                     selectImage.setImageBitmap(photo);
                     storeCropImage(photo, filepath);
                     absoultePath = filepath;
-                    //System.out.println("#########FILEPATH:" + absoultePath);
+                    System.out.println("#########FILEPATH:" + absoultePath);
+                } catch (IOException e) { e.printStackTrace(); }
 
-                }
                 break;
             case 3:
                 Intent intents = data;
-                String url = intents.getStringExtra("url");
-                styleURL = url;
-                Glide.with(ImageTransferActivity.this).load(url).into(styleimage);
+                String url = null;
+                try {
+                    url = intents.getStringExtra("url");
+                    System.out.println(url);
+                    styleURL = url;
+                    Glide.with(ImageTransferActivity.this).load(url).into(styleimage);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
                 break;
 
+            case CAMERA_REQUEST_CODE:
+                try {
+                    photoUri = data.getData();
+                } catch (Exception e) {
+                    e.printStackTrace(); break;
+                }
+
+                Intent cameraIntent = new Intent("com.android.camera.action.CROP");
+                cameraIntent.setDataAndType(photoUri, "image/*");
+
+                startActivityForResult(cameraIntent, 2);
             default:
                 System.out.println("!!!!!1취소");
                 break;
